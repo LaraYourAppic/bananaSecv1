1b6c1bf7e248a1e00ba28ac82d9cb50fb2175f68
LaraYourAppic <Lara@YourAppic.com>
2022-04-06 16:44:12 +0200

    huiswerk opdracht

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import logo from '../assets/banana-01.png';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory, Link,} from 'react-router-dom';
+import {AuthContext} from "../context/AuthContext/AuthContext";
 
 function NavBar() {
   const history = useHistory();
+  const {auth, logout} = useContext(AuthContext)
+  console.log(auth);
 
   return (
     <nav>
@@ -17,6 +20,7 @@ function NavBar() {
         </Link>
 
       <div>
+        {!auth ? <>
         <button
           type="button"
           onClick={() => history.push('/signin')}
@@ -29,6 +33,13 @@ function NavBar() {
         >
           Registreren
         </button>
+        </> : <button
+            type= "button"
+        onClick={logout}
+        >LogOut
+        </button>
+        }
+
       </div>
     </nav>
   );
